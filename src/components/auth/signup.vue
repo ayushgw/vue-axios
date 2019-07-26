8e48d53ba0da094b61ed90db41ab47497be8b6b8
ayushgw <ayushgoswami@zoho.com>
2019-07-26 23:33:00 +0530

    signup form validation done

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -2,21 +2,45 @@
   <div id="signup">
     <div class="signup-form">
       <form @submit.prevent="onSubmit">
-        <div class="input">
+        <div class="input" :class="{'invalid': $v.email.$error}">
           <label for="email">Mail</label>
-          <input type="email" id="email" v-model="email" />
+          <input type="email" id="email" @blur="$v.email.$touch()" v-model="email" />
+          <transition
+            name="custom-classes-transition"
+            enter-active-class="animated jello"
+            leave-active-class="animated bounceOutUp"
+            mode="out-in"
+          >
+            <p v-if="!$v.email.email">Please provide a valid email.</p>
+            <p v-if="!$v.email.required">Please fill in your email.</p>
+          </transition>
         </div>
-        <div class="input">
+        <div class="input" :class="{'invalid': $v.age.$error}">
           <label for="age">Your Age</label>
-          <input type="number" id="age" v-model.number="age" />
+          <input type="number" id="age" @blur="$v.age.$touch()" v-model.number="age" />
+          <transition
+            name="custom-classes-transition"
+            enter-active-class="animated jello"
+            leave-active-class="animated bounceOutUp"
+            mode="out-in"
+          >
+            <p
+              v-if="!$v.age.minVal && $v.age.$error"
+            >Should be atleast {{ $v.age.$params.minVal.min }} years old.</p>
+          </transition>
         </div>
-        <div class="input">
+        <div class="input" :class="{'invalid': $v.password.$error}">
           <label for="password">Password</label>
-          <input type="password" id="password" v-model="password" />
+          <input type="password" id="password" @blur="$v.password.$touch()" v-model="password" />
         </div>
-        <div class="input">
+        <div class="input" :class="{'invalid': $v.confirmPassword.$error}">
           <label for="confirm-password">Confirm Password</label>
-          <input type="password" id="confirm-password" v-model="confirmPassword" />
+          <input
+            type="password"
+            id="confirm-password"
+            @blur="$v.confirmPassword.$touch()"
+            v-model="confirmPassword"
+          />
         </div>
         <div class="input">
           <label for="country">Country</label>
@@ -31,19 +55,41 @@
           <h3>Add some Hobbies</h3>
           <button @click="onAddHobby" type="button">Add Hobby</button>
           <div class="hobby-list">
-            <div class="input" v-for="(hobbyInput, index) in hobbyInputs" :key="hobbyInput.id">
+            <div
+              class="input"
+              v-for="(hobbyInput, index) in hobbyInputs"
+              :class="{invalid: $v.hobbyInputs.$each[index].$error}"
+              :key="hobbyInput.id"
+            >
               <label :for="hobbyInput.id">Hobby #{{ index }}</label>
-              <input type="text" :id="hobbyInput.id" v-model="hobbyInput.value" />
+              <input
+                type="text"
+                :id="hobbyInput.id"
+                @blur="$v.hobbyInputs.$each[index].value.touch()"
+                v-model="hobbyInput.value"
+              />
               <button @click="onDeleteHobby(hobbyInput.id)" type="button">X</button>
             </div>
+            <transition
+              name="custom-classes-transition"
+              enter-active-class="animated jello"
+              leave-active-class="animated bounceOutUp"
+              mode="out-in"
+            >
+              <p
+                v-if="!$v.hobbyInputs.minLen" class="text-danger"
+              >Should input atleast {{ $v.hobbyInputs.$params.minLen.min }} hobbies.</p>
+            </transition>
           </div>
         </div>
-        <div class="input inline">
-          <input type="checkbox" id="terms" v-model="terms" />
-          <label for="terms">Accept Terms of Use</label>
+        <div class="input inline" :class="{invalid: $v.terms.$error}">
+          <input type="checkbox" id="terms" @change="$v.terms.$touch()" v-model="terms" />
+          <label for="terms" :class="{'text-danger': $v.terms.$error}">Accept Terms of Use</label>
+          <!-- {{terms}}, {{$v.terms}}, {{$v.terms.$error}} -->
+          {{terms}}
         </div>
         <div class="submit">
-          <button type="submit">Submit</button>
+          <button type="submit" :disabled="$v.$invalid">Submit</button>
         </div>
       </form>
     </div>
@@ -51,6 +97,16 @@
 </template>
 
 <script>
+import {
+  required,
+  email,
+  numeric,
+  minValue,
+  minLength,
+  sameAs,
+  requiredUnless
+} from "vuelidate/lib/validators";
+
 export default {
   data() {
     return {
@@ -62,6 +118,49 @@ export default {
       hobbyInputs: [],
       terms: false
     };
+  },
+  computed: {
+    isIndia() {
+      return this.country === "india";
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
+    },
+    age: {
+      required,
+      numeric,
+      minVal: minValue(18)
+    },
+    password: {
+      required,
+      minLen: minLength(6)
+    },
+    confirmPassword: {
+      // sameAs: sameAs('password')
+      //  OR
+      sameAs: sameAs(vm => {
+        return vm.password;
+      })
+    },
+    terms: {
+      required: requiredUnless("isIndia"), //not working anymore
+      sameAs: sameAs(() => {
+        return true;
+      })
+    },
+    hobbyInputs: {
+      required,
+      minLen: minLength(2),
+      $each: {
+        value: {
+          required,
+          minLen: minLength(5)
+        }
+      }
+    }
   },
   methods: {
     onAddHobby() {
@@ -176,5 +275,14 @@ export default {
   background-color: transparent;
   color: #ccc;
   cursor: not-allowed;
+}
+
+.input.invalid p {
+  color: red;
+}
+
+.input.invalid input {
+  border: 1px solid red;
+  box-shadow: 0px 0px 12px rgb(225, 131, 81);
 }
 </style>
